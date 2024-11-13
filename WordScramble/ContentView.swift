a1c9db2c0ecf8501a0d7a81b1753e2630e70ef3e
Constantin Lisnic <constantin@lisnic.dev>
2024-11-13 15:25:17 +0000

    project setup

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -5,17 +5,21 @@
 //  Created by Constantin Lisnic on 13/11/2024.
 //
 
+import Inject
 import SwiftUI
 
 struct ContentView: View {
+    @ObserveInjection var inject
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("this damn")
         }
         .padding()
+        .enableInjection()
     }
 }
 
